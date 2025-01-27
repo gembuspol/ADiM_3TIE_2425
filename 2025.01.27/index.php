3a139afdfa53fd37e03e3840e5896ae7acde89a1
Przemysław Gembalczyk <gembuspol@interia.pl>
2025-01-27 14:58:36 +0100

    a
    Signed-off-by: Przemysław Gembalczyk <gembuspol@interia.pl>

--- 2025.01.27/index.php
+++ 2025.01.27/index.php
@@ -1,11 +1,30 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Pierwszy program w PHP</title>
 </head>
 <body>
-    
+    <header>
+        Pierwszy program
+    </header>
+    <main>
+        <?php
+            echo "Witajcie w świecie PHP";
+            echo "<br>";
+            echo 5+3/5*2;
+            $skibidi=55;
+            $tego=32;
+            echo "<br>Iloraz liczb ".$skibidi." i ".$tego." wynosi: ";
+           
+            echo $skibidi/$tego;
+            $imie="Przemek";
+            echo "<br>Witaj na stronie, $imie";
+        ?>
+    </main>
+    <footer>
+        Stronę wykonał Przemek
+    </footer>
 </body>
 </html>
