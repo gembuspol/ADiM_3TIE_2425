dd721c00423f4d7d117e8ee8d5416d35e7da38bf
Przemyslaw Gembalczyk <gembuspol@interia.pl>
2025-06-06 13:29:48 +0200

    a

---
+++ 2025-06-05/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <h1>Kalkulator kosztów montażu</h1>
+        <label for="szerokosc">Szerokość pomieszczenia [m]: </label>
+        <input type="number" id="szerokosc" name="szerokosc">
+        <label for="dlugosc">Długość pomieszczenia [m]: </label>
+        <input type="number" id="dlugosc" name="dlugosc">
+        <p>Typ panelu</p>
+        <label for="laminowane">Panele laminowane</label>
+        <input type="radio" id="laminowane" name="radio" value="12">
+        <br>
+         <label for="winylowe">Panele winylowe</label>
+        <input type="radio" id="winylowe" name="radio" value="14">
+        <br>
+         <label for="deska">Deska podłogowa</label>
+        <input type="radio" id="deska" name="radio" value="18">
+        <br>
+
+    </main>
+</body>
+</html>
